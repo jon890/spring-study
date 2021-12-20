1c2a32b68c3401941617d956c4ac46c68833399c
jon890 <jon89071@gmail.com>
2021-12-20 20:18:33 +0900

    - spring security project init

---
+++ spring-security/build.gradle.kts
@@ -0,0 +1,67 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.6.1"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+
+    kotlin("jvm") version "1.6.0"
+    kotlin("plugin.spring") version "1.6.0"
+    kotlin("plugin.jpa") version "1.6.0"
+}
+
+group "com.bifos"
+version "1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // spring
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // hateoas
+    implementation("org.springframework.boot:spring-boot-starter-hateoas")
+    implementation("org.springframework.hateoas:spring-hateoas")
+
+    // openapi
+    // todo kbt : version 은 외부에서 지정할 수 있게
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.1")
+    // implementation("org.springdoc:springdoc-openapi-webmvc-core:1.6.1")
+    // implementation("org.springdoc:springdoc-openapi-hateoas:1.6.1")
+    // implementation("org.springdoc:springdoc-openapi-security:1.6.1")
+    // implementation("org.springdoc:springdoc-openapi-kotlin:1.6.1")
+
+    // security - core, config, web 포함
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // kotlin
+    implementation(kotlin("reflect"))
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("stdlib-jdk8"))
+
+    // db
+    runtimeOnly("com.h2database:h2")
+
+    // dev only
+    // spring dev tools
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    // test
+    // spring-test, json-path, junit, assertj-core, mockito-core, hamcrest-core, jsonassert 포함
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
