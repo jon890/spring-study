3ae58013eebfaec76de599952fdccb07dc56d2fe
BiFoS <jon89071@gmail.com>
2022-01-14 14:38:23 +0900

    Fix : Jdbc, Jpa 프로젝트 분리

--- spring-security/src/main/resources/database/h2/calendar-bcrypt.sql
+++
@@ -1,6 +0,0 @@
--- original password was: user1
-update calendar_users set password = '$2a$10$zJUcp7.mQuZj6OObZ77SAO5w4oJumBg0OyQ0yfemsBoAab.EXQpyS' where email = 'user1@example.com';
--- original password was: admin1
-update calendar_users set password = '$2a$10$rPlvGYo9CqWd/rEydVEC7emJ9gj1MMLJZG7vNuO7LfEOo.qvphexe' where email = 'admin1@example.com';
--- original password was: user2
-update calendar_users set password = '$2a$10$hZ.uCjYx6wi8ZeYUGf1kMeUPQcjbAqgKmHtIlXdOEF6BdcbZ/4kp6' where email = 'user2@example.com';
